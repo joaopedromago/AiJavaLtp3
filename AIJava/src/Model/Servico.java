a6cd887c5e522f33887ad8f62743a8b09a78fa2e
João Pedro Ferreira Maciel <zaconou@gmail.com>
2018-05-10 21:17:25 -0300

    Creating Project

---
+++ AIJava/src/Model/Servico.java
@@ -0,0 +1,33 @@
+package Model;
+
+import Enum.TipoServico;
+
+public class Servico extends BaseModel {
+	private String nome;
+	private double preco;
+	private TipoServico tipoServico;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public TipoServico getTipoServico() {
+		return tipoServico;
+	}
+
+	public void setTipoServico(TipoServico tipoServico) {
+		this.tipoServico = tipoServico;
+	}
+}
