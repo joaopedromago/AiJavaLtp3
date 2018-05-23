cb47ed4845ac18e83369d438a94cdccb5076402e
João Pedro Ferreira Maciel <zaconou@gmail.com>
2018-05-22 22:05:03 -0300

    Criando repo padrão e adicionando telas básicas
    aula de 22/05/2018

--- AIJava/src/Infra/DbContext.java
+++ AIJava/src/Infra/DbContext.java
@@ -8,16 +8,67 @@ import java.util.ArrayList;
 
 public class DbContext {
 
-      Connection c = null;      
-      
     public DbContext() {
-      try {
-         Class.forName("org.sqlite.JDBC");
-         c = DriverManager.getConnection("jdbc:sqlite:test.db");
-      } catch ( Exception e ) {
-         System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-         System.exit(0);
-      }
-      System.out.println("Opened database successfully");
+
+    }
+
+    public boolean ExecuteQuery(String query) {
+        Statement stmt = null;
+        try {
+            Connection c = Open();
+            stmt = c.createStatement();
+
+            stmt.executeUpdate(query);
+            stmt.close();
+
+            c.commit();
+            c.close();
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+        System.out.println("Records created successfully");
+
+        return true;
+    }
+
+    public ResultSet ExecuteQuerySelect(String query) {
+
+        Statement stmt = null;
+        try {
+            Connection c = Open();
+            stmt = c.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+
+            rs.close();
+            stmt.close();
+            c.close();
+            System.out.println("Operation done successfully");
+            return rs;
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+        return null;
+    }
+
+    public Connection Open() {
+
+        try {
+            Connection c = null;
+
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:test.db");
+            c.setAutoCommit(false);
+
+            System.out.println("Opened database successfully");
+
+            return c;
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+
+        return null;
     }
 }
--- AIJava/src/Repository/ClienteRepository.java
+++ AIJava/src/Repository/ClienteRepository.java
@@ -1,1 +1,117 @@
 package Repository;
+
+import Infra.DbContext;
+import Model.Cliente;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ClienteRepository {
+
+    DbContext db;
+
+    public ClienteRepository() {
+        db = new DbContext();
+    }
+
+    public boolean salvar(Cliente cliente) {
+        try {
+            String query = "INSERT INTO CLIENTES "
+                    + "(pessoaId, numeroCompras) "
+                    + "VALUES (" + cliente.getPessoaId() + ", " + cliente.getNumeroCompras() + ")";
+
+            return db.ExecuteQuery(query);
+
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public boolean atualizar(Cliente cliente) {
+        try {
+            String query = "UPDATE CLIENTES "
+                    + "set pessoaId = " + cliente.getPessoaId() + ", "
+                    + "numeroCompras = " + cliente.getNumeroCompras()
+                    + "where id = " + cliente.getId();
+
+            return db.ExecuteQuery(query);
+
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public boolean remover(int id) {
+        try {
+            String query = "DELETE CLIENTES "
+                    + "where id = " + id;
+
+            return db.ExecuteQuery(query);
+
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public Cliente obterCliente(int id) {
+        try {
+            String query = "SELECT * FROM CLIENTES "
+                    + "WHERE ID = " + id;
+
+            Cliente cliente = new Cliente();
+
+            ResultSet rs = db.ExecuteQuerySelect(query);
+            while (rs.next()) {
+                cliente = obterCliente(rs);
+            }
+
+            return cliente;
+
+        } catch (SQLException e) {
+            try {
+                throw e;
+            } catch (SQLException ex) {
+                Logger.getLogger(ClienteRepository.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+        return null;
+    }
+
+    public List<Cliente> obterCliente() {
+        try {
+            List<Cliente> clientes = new ArrayList<Cliente>();
+
+            String query = "SELECT * FROM CLIENTES ";
+
+            ResultSet rs = db.ExecuteQuerySelect(query);
+            while (rs.next()) {
+                Cliente cliente = obterCliente(rs);
+                clientes.add(cliente);
+            }
+
+            return clientes;
+        } catch (Exception e) {
+            try {
+                throw e;
+            } catch (Exception ex) {
+                Logger.getLogger(ClienteRepository.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return null;
+    }
+
+    private Cliente obterCliente(ResultSet rs) throws SQLException {
+        Cliente cliente = new Cliente();
+        cliente.setId(rs.getInt("Id"));
+        cliente.setDataCriacao(rs.getDate("DataCriacao"));
+        cliente.setDataExclusao(rs.getDate("DataExclusao"));
+        cliente.setNumeroCompras(rs.getInt("NumeroCompras"));
+        cliente.setPessoaId(rs.getInt("PessoaId"));
+
+        return cliente;
+    }
+}
