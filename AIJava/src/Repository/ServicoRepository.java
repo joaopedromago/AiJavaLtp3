b54d644ee961b66b7fa62f94eb583873ae0f92bc
Mago <zaconou@gmail.com>
2018-06-10 22:40:28 -0300

    repositórios criados

--- AIJava/src/Repository/ServicoRepository.java
+++ AIJava/src/Repository/ServicoRepository.java
@@ -1,1 +1,120 @@
 package Repository;
+
+import Enum.TipoServico;
+import Infra.DbContext;
+import Model.Servico;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ServicoRepository {
+
+    DbContext db;
+
+    public ServicoRepository() {
+        db = new DbContext();
+    }
+
+    public boolean salvar(Servico servico) {
+        try {
+            String query = "INSERT INTO SERVICOS "
+                    + "(nome,preco,tiposervico) "
+                    + "VALUES (" + servico.getNome() + "," + servico.getPreco()
+                    + "," + servico.getTipoServico() + ")";
+
+            return db.ExecuteQuery(query);
+
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public boolean atualizar(Servico servico) {
+        try {
+            String query = "UPDATE SERVICOS set "
+                    + " nome = " + servico.getNome() + ", preco = " + servico.getPreco()
+                    + ", tiposervico = " + servico.getTipoServico()
+                    + " WHERE id = " + servico.getId();
+
+            return db.ExecuteQuery(query);
+
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public boolean remover(int id) {
+        try {
+            String query = "DELETE SERVICOS "
+                    + "where id = " + id;
+
+            return db.ExecuteQuery(query);
+
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public Servico obterServico(int id) {
+        try {
+            String query = "SELECT * FROM SERVICOS "
+                    + "WHERE ID = " + id;
+
+            Servico servico = new Servico();
+
+            ResultSet rs = db.ExecuteQuerySelect(query);
+            while (rs.next()) {
+                servico = obterServico(rs);
+            }
+
+            return servico;
+
+        } catch (SQLException e) {
+            try {
+                throw e;
+            } catch (SQLException ex) {
+                Logger.getLogger(ServicoRepository.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+        return null;
+    }
+
+    public List<Servico> obterServico() {
+        try {
+            List<Servico> servicos = new ArrayList<Servico>();
+
+            String query = "SELECT * FROM SERVICOS ";
+
+            ResultSet rs = db.ExecuteQuerySelect(query);
+            while (rs.next()) {
+                Servico servico = obterServico(rs);
+                servicos.add(servico);
+            }
+
+            return servicos;
+        } catch (Exception e) {
+            try {
+                throw e;
+            } catch (Exception ex) {
+                Logger.getLogger(ServicoRepository.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return null;
+    }
+
+    private Servico obterServico(ResultSet rs) throws SQLException {
+        Servico servico = new Servico();
+        servico.setId(rs.getInt("id"));
+        servico.setDataCriacao(rs.getDate("dataCriacao"));
+        servico.setDataExclusao(rs.getDate("dataExclusao"));
+        servico.setNome(rs.getString("nome"));
+        servico.setPreco(rs.getDouble("preco"));
+        servico.setTipoServico(TipoServico.values()[rs.getInt("tipoServico")]);
+
+        return servico;
+    }
+}
--- AIJava/src/Repository/ServicoVendedorRepository.java
+++ AIJava/src/Repository/ServicoVendedorRepository.java
@@ -1,1 +1,120 @@
 package Repository;
+
+import Infra.DbContext;
+import Model.ServicoVendedor;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ServicoVendedorRepository {
+
+    DbContext db;
+
+    public ServicoVendedorRepository() {
+        db = new DbContext();
+    }
+
+    public boolean salvar(ServicoVendedor servicoVendedor) {
+        try {
+            String query = "INSERT INTO SERVICOSVENDEDORESS "
+                    + "(quantidadeServicos,servicoId,vendedorId) "
+                    + "VALUES (" + servicoVendedor.getQuantidadeServicos() + "," + servicoVendedor.getServicoId()
+                    + "," + servicoVendedor.getVendedorId() + ")";
+
+            return db.ExecuteQuery(query);
+
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public boolean atualizar(ServicoVendedor servicoVendedor) {
+        try {
+            String query = "UPDATE SERVICOSVENDEDORESS set "
+                    + " quantidadeServicos = " + servicoVendedor.getQuantidadeServicos()
+                    + ", servicoId = " + servicoVendedor.getServicoId()
+                    + ", vendedorId = " + servicoVendedor.getVendedorId()
+                    + " WHERE id = " + servicoVendedor.getId();
+
+            return db.ExecuteQuery(query);
+
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public boolean remover(int id) {
+        try {
+            String query = "DELETE SERVICOSVENDEDORESS "
+                    + "where id = " + id;
+
+            return db.ExecuteQuery(query);
+
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public ServicoVendedor obterServicoVendedor(int id) {
+        try {
+            String query = "SELECT * FROM SERVICOSVENDEDORESS "
+                    + "WHERE ID = " + id;
+
+            ServicoVendedor servicoVendedor = new ServicoVendedor();
+
+            ResultSet rs = db.ExecuteQuerySelect(query);
+            while (rs.next()) {
+                servicoVendedor = obterServicoVendedor(rs);
+            }
+
+            return servicoVendedor;
+
+        } catch (SQLException e) {
+            try {
+                throw e;
+            } catch (SQLException ex) {
+                Logger.getLogger(ServicoVendedorRepository.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+        return null;
+    }
+
+    public List<ServicoVendedor> obterServicoVendedor() {
+        try {
+            List<ServicoVendedor> servicoVendedors = new ArrayList<ServicoVendedor>();
+
+            String query = "SELECT * FROM SERVICOSVENDEDORESS ";
+
+            ResultSet rs = db.ExecuteQuerySelect(query);
+            while (rs.next()) {
+                ServicoVendedor servicoVendedor = obterServicoVendedor(rs);
+                servicoVendedors.add(servicoVendedor);
+            }
+
+            return servicoVendedors;
+        } catch (Exception e) {
+            try {
+                throw e;
+            } catch (Exception ex) {
+                Logger.getLogger(ServicoVendedorRepository.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return null;
+    }
+
+    private ServicoVendedor obterServicoVendedor(ResultSet rs) throws SQLException {
+        ServicoVendedor servicoVendedor = new ServicoVendedor();
+        servicoVendedor.setId(rs.getInt("id"));
+        servicoVendedor.setDataCriacao(rs.getDate("dataCriacao"));
+        servicoVendedor.setDataExclusao(rs.getDate("dataExclusao"));
+        servicoVendedor.setQuantidadeServicos(rs.getInt("quantidadeServicos"));
+        servicoVendedor.setServicoId(rs.getInt("servicoId"));
+        servicoVendedor.setVendedorId(rs.getInt("vendedorId"));
+
+        return servicoVendedor;
+    }
+}
